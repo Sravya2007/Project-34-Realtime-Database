7b43ad7e898ef65b35796494d06300ddbea79faf
Sravya2007 <sravyamarepally@outlook.com>
2020-07-01 13:29:19 +0530

    Change in everything

--- sketch.js
+++ sketch.js
@@ -3,34 +3,38 @@ var database, position;
 
 function setup(){
     database = firebase.database();
-    console.log(database);
-    ball = createSprite(250,250,10,10);
+    //console.log(database);
+
+    ball = createSprite(200,200,10,10);
     ball.shapeColor = "red";
+
     var ballPosition = database.ref('ball/position');
     ballPosition.on("value", readPosition, showError);
+    //createCanvas(500, 500);
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-
+    
+    if(position !== undefined){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){           
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+1);
+        }
+    }   
     drawSprites();
 }
 
 function readPosition(data) {
     position = data.val();
-    console.log(position);
     ball.x = position.x;
     ball.y = position.y;
 }
@@ -46,7 +50,3 @@ function showError() {
     text("ERROR", 200, 200);
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
-}
